8ff3bfffe7910df075482ec2fa029ec31bce8cee
Kun Woo Kim <kimkuns98@gmail.com>
2024-07-25 17:44:09 +0900

    test

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -1,19 +1,27 @@
 import VideoContainer from "@/components/VideoContainer";
 
+// const video = {
+//   sources: [
+//     {
+//       src: "https://storage.googleapis.com/white_mouse_dev/videos/movies/thegarfieldmovie/thegarfieldmovie.mp4",
+//       type: "video/mp4",
+//     },
+//   ],
+//   tracks: [
+//     {
+//       kind: "captions",
+//       src: "https://storage.googleapis.com/white_mouse_dev/videos/movies/thegarfieldmovie/thegarfieldmovie.vtt",
+//       srclang: "kr",
+//       label: "Korean",
+//       default: true,
+//     },
+//   ],
+// };
 const video = {
   sources: [
     {
-      src: "https://storage.googleapis.com/white_mouse_dev/videos/movies/thegarfieldmovie/thegarfieldmovie.mp4",
+      src: "https://storage.googleapis.com/white_mouse_dev/videos/movies/r18idolseungh/r18idolseungh.mp4",
       type: "video/mp4",
-    },
-  ],
-  tracks: [
-    {
-      kind: "captions",
-      src: "https://storage.googleapis.com/white_mouse_dev/videos/movies/thegarfieldmovie/thegarfieldmovie.vtt",
-      srclang: "kr",
-      label: "Korean",
-      default: true,
     },
   ],
 };
